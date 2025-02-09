62c43536a749491e1c48e320d9cb3d43afe993f6
alyssalavalle <alavalle3521@sdsu.edu>
2025-02-08 20:47:58 -0800

    Completed HW Assignment

--- linkedlist.py
+++ linkedlist.py
@@ -1,6 +1,5 @@
 import Spaceship
 
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -8,7 +7,6 @@ class Node:
 
     def __str__(self):
         return str(self.value)
-
 
 class LinkedList:
     def __init__(self, value):
@@ -76,20 +74,73 @@ class LinkedList:
             temp = temp.next
 
 
-# TODO : Write function insertatindex to insert a newnode at any given index. Consider all edge cases, including missing nodes.
-# TODO : Write fucntion deleteatindex to delete a newnode at any given index. COnsider all edge cases, including missing nodes.
+# TODO : Write function insert_at_index to insert a new node at any given index. Consider all edge cases, including missing nodes.
+    def insert_at_index(self, index, value):
+        # IF OUT OF BOUNDS, DON'T MODIFY
+        if index < 0 or index > self.length:
+            return False
+        # ADDS NEW VALUE
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        # MODIFIES LIST
+        new_node = Node(value)
+        temp = self.head
+        for i in range(index - 1):
+            temp = temp.next
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+# TODO : Write function delete_at_index to delete a new node at any given index. Consider all edge cases, including missing nodes.
 # Make sure to reuse existing function for the correct edge cases for both TODOs
 # Write appropriate test function below to test for the new functions.
+    def delete_at_index(self, index):
+        # CHECKS BOUNDS
+        if index < 0 or index >= self.length:
+            return None
+        # DELETES VALUE
+        if index == 0:
+            return self.delfirst()
+        if index == self.length - 1:
+            return self.dellast()
 
-s1 = Spaceship("Voyager", 300)
-s2 = Spaceship("Enterprise", 300)
-s3 = Spaceship("Atlantis", 300)
-s4 = Spaceship("Challenger", 300)
-s5 = Spaceship("Artemis", 300)
+        # MODIFIES LIST
+        prev = self.head
+        for j in range(index - 1):
+            prev = prev.next
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+
+s1 = Spaceship.Spaceship("Voyager", 300)
+s2 = Spaceship.Spaceship("Enterprise", 300)
+s3 = Spaceship.Spaceship("Atlantis", 300)
+s4 = Spaceship.Spaceship("Challenger", 300)
+s5 = Spaceship.Spaceship("Artemis", 300)
 
 mylinkedlist = LinkedList(s1)
+# FIRST LIST
 mylinkedlist.append(s2)
 mylinkedlist.append(s3)
 mylinkedlist.prepend(s4)
 mylinkedlist.prepend(s5)
 mylinkedlist.print_list()
+print("\n")
+
+# TEST CASE W/ INSERT INDEX
+mylinkedlist.insert_at_index(1, Spaceship.Spaceship("RANDOM", 200))
+mylinkedlist.print_list()
+print("\n")
+# Expected: Artemis → Challenger → Voyager → Enterprise → Atlantis
+
+# TEST CASE W/ DELETE INDEX
+mylinkedlist.delete_at_index(2)
+mylinkedlist.print_list()
+# Expected: Artemis → Challenger → Enterprise → Atlantis
+
